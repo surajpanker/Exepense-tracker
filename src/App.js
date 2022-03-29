d2a2e995859537751734b2813986254581527fe2
surajpanker82 <surajpanker82@gmail.com>
2022-03-29 11:53:53 +0530

    code is added

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+
+import Expenses from './components/Expenses/Expenses';
+
+
+let DUMMY_EXPENSE = [
+        
+];
+
+const App = () => {
+    
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
+    function fetchData(){
+        fetch('https://techgun.website/sample/api/read.php').then(
+            response => {
+                return response.json();
+            }
+        ).then(
+            data => {
+                //console.log(data);
+                setExpenses(data);
+            }
+        );
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    //added code
+    const addExpenseHandler = (expense) => {
+        setExpenses([expense,...expenses])
+    };
+
+    return (
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expenses item={expenses} />
+        </div>    
+    );
+}
+
+export default App;
